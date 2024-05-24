1515bfd0ae9cc9b1da98a8481d7d647c8218002e
Dheeman Das <152193475+Dheemaan29@users.noreply.github.com>
2024-05-24 18:42:07 +0530

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,5 +1,4 @@
 # main_app.py
-
 import streamlit as st
 import pandas as pd
 import azuresql_new
@@ -7,37 +6,7 @@ from prompts.prompts import SYSTEM_MESSAGE
 from azure_openai import get_completion_from_messages
 import json
 import re
-from dotenv import load_dotenv
-import os
-import openai
 
-load_dotenv()
-
-openai.api_type = "azure"
-openai.api_base = "https://aipoctesting.openai.azure.com/"
-openai.api_version = "2023-10-01-preview"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def get_completion_from_messages(system_message, user_message, model="gpt-35-turbo", temperature=0, max_tokens=500) -> str:
-
-    messages = [
-        {'role': 'system', 'content': system_message},
-        {'role': 'user', 'content': f"{user_message}"}
-    ]
-    
-    response = openai.ChatCompletion.create(
-        engine=model,
-        messages=messages,
-        temperature=temperature, 
-        max_tokens=max_tokens, 
-    )
-    
-    return response.choices[0].message["content"]
-
-if __name__ == "__main__":
-    system_message = "You are a helpful assistant"
-    user_message = "Hello, how are you?"
-    print(get_completion_from_messages(system_message, user_message))
 
 def query_database(query, conn):
     """Run SQL query and return results in a dataframe"""
